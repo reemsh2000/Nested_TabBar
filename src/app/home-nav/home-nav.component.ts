973e1461d2dc485ddcee4b099871b9f1c40158f6
reemsh2000 <sh951652@gmail.com>
2022-12-09 12:22:13 +0200

    create home navbar

---
+++ src/app/home-nav/home-nav.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit,ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-home-nav',
+  templateUrl: './home-nav.component.html',
+  styleUrls: ['./home-nav.component.scss'],
+ // encapsulation: ViewEncapsulation.None
+
+})
+export class HomeNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
