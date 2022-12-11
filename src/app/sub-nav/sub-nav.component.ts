1261f5952ef194fc2fe3f9014ecd415b288c3c75
reemsh2000 <sh951652@gmail.com>
2022-12-11 15:39:33 +0200

    add split button

--- src/app/sub-nav/sub-nav.component.ts
+++ src/app/sub-nav/sub-nav.component.ts
@@ -8,16 +8,20 @@ import { MenuItem } from 'primeng/api';
 })
 export class SubNavComponent implements OnInit {
   @Input() items: any[] = [];
-  docItems: any = [
-    { label: 'Clearance Alerts' },
-    { label: 'Certifications' },
-    { label: 'Bank Details' },
-    { label: 'Legal Docs' },
-    { label: 'Other Docs' },
+  index: number = 0;
+  selectedTab: any = 'Clearance Alerts';
+  docItems: MenuItem[] = [
+    { label: 'Clearance Alerts', command: () => (this.index = 0) },
+    { label: 'Certifications', command: () => (this.index = 1) },
+    { label: 'Bank Details', command: () => (this.index = 2) },
+    { label: 'Legal Docs', command: () => (this.index = 3) },
+    { label: 'Other Docs', command: () => (this.index = 4) },
   ];
-  constructor() {
-    console.log({ it: this.items });
-  }
+  constructor() {}
 
   ngOnInit(): void {}
+
+  func(e: any) {
+    console.log('hi', e);
+  }
 }
