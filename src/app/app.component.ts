3ce9d5e6a3de87ea588368d6e9ad35ba8fd81beb
reemsh2000 <sh951652@gmail.com>
2022-12-12 13:11:14 +0200

    check dropdown test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,16 @@ import { MenuItem } from 'primeng/api';
 })
 export class AppComponent {
   title = 'tab_';
+  selectedTab: any = 'Clearance Alerts';
+  index: number = 0;
+
+  docItems: MenuItem[] = [
+    { label: 'Clearance Alerts', command: () => (this.index = 0) },
+    { label: 'Certifications', command: () => (this.index = 1) },
+    { label: 'Bank Details', command: () => (this.index = 2) },
+    { label: 'Legal Docs', command: () => (this.index = 3) },
+    { label: 'Other Docs', command: () => (this.index = 4) },
+  ];
   ngOnInit() {
   }
 }
